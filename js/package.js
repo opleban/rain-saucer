d1c8fe72b9a3a96c5b0b0cac74706ea3e1630346
Ori Pleban <opleban@gmail.com>
2015-04-14 18:40:36 -0400

    adds other surface area

--- js/package.js
+++ js/package.js
@@ -47,7 +47,8 @@
 	var $rainSaucers = $('.rainSaucer');
 	var $rainPotential = $('.rain-potential');
 	var $averageRainfall = $('.average-rainfall');
-	var $rainfallCapture = $('.rain-collected');
+	var $rainfallCapturePotential = $('.rain-collected');
+	var $otherSurfaceArea = $('custom-surface-area');
 
 	$.getJSON('data/rainfallByCity.json', function(rainfallData){
 		for (var i=0; i<rainfallData.length; i++){
@@ -65,20 +66,20 @@
 	}
 
 	var calculateRainSaucerCapacity = function(){
-		var totalRainsaucerCapacity = 0;
+		var totalRainsaucerCapicity = 0;
 		for(var i=0; i<$rainSaucers.length; i++){
 			var rainsaucer = $rainSaucers[i]
-			var rainsaucerCapacity = +$(rainsaucer).attr('data-capacity');
-			totalRainsaucerCapacity += rainsaucer.value * rainsaucerCapacity;
+			var captureRate = +$(rainsaucer).attr('data-capacity');
+			totalRainsaucerCapacity += rainsaucer.value * captureRate;
 		}
-		renderRainPotential(totalRainsaucerCapacity);
+		renderRainCapacity(totalRainsaucerCapacity);
 	};
 
-	var renderRainPotential = function(n){
+	var renderRainCapacity = function(n){
 		$rainPotential.html(n.toFixed(2));
 	};
 
-	var renderRainfallCapture = function(n){
+	var renderRainfallCapturePotential = function(n){
 		$rainfallCapture.html(n.toFixed(2));
 	};
 
@@ -86,16 +87,22 @@
 		$averageRainfall.html(n);
 	}
 
-	var calculateRainfallCapture = function(){
+	var calculateRainfallCapturePotential = function(){
 		var rainPotential = +($rainPotential.html());
 		var averageRainfall = +($averageRainfall.html());
 		return rainPotential * averageRainfall;
 	}
 
+	var calculateOtherSurfaceAreaCapturePotential = function(){
+		var otherSurfaceArea = $otherSurfaceArea.value();
+		var captureRate = +$($otherSurfaceArea.attr('data-capacity'));
+		var rainCapturePotential = $otherSurfaceArea.value * captureRate
+	}
+
 	$rainSaucers.on('input', function(){
 		calculateRainSaucerCapacity();
-		var rainfallCapture = calculateRainfallCapture();
-		renderRainfallCapture(rainfallCapture);	
+		var rainfallCapture = calculateRainfallCapturePotential();
+		renderRainfallCapturePotential(rainfallCapturePotential);	
 	});
 
 	$cityOptions.on('change', function(){
